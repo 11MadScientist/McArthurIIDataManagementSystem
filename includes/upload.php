3db41f71a5efe1d262aab77161ca90bd2201a650
febieclinton <72293441+febieclinton@users.noreply.github.com>
2020-11-02 14:02:24 +0800

    Updated upload.php
    Withe essential comments

--- includes/upload.php
+++ includes/upload.php
@@ -1,16 +1,25 @@
- <?php   
- 
- $output = '';  
- if(isset($_FILES['file']['name'][0]))  
- {  
-      //uploading the files in the upload folder which is a new folder created by me
-      foreach($_FILES['file']['name'] as $keys => $values)  
-      {  
-           if(move_uploaded_file($_FILES['file']['tmp_name'][$keys], 'upload/' . $values))  
-           {  
-                $output .= '<div class=col-md-3"><img src="upload/'.$values.'" class="img-responsive" /></div>';  
-           }  
-      }  
- }  
- echo $output;  
- ?>  
+ <?php
+//       refers to the file array in formData.append()
+//                  V
+//                  V    refers to the filename uploaded 
+//                  V       V
+//                  V       V
+//                  V       V
+ if(isset($_FILES['file']['name']))
+ {
+     //uploading the files in the upload folder which is a new folder created by me
+     foreach($_FILES['file']['name'] as $keys => $values)
+     {
+          // SAVE TO THE UPLOAD FOLDER DIRECTLY       INDEX IN FILE ARRAY
+          //                                                 V
+          if(move_uploaded_file($_FILES['file']['tmp_name'][$keys], 'upload/' . $values))
+          {
+               echo "File Upload Success";
+          }
+          else
+          {
+               echo "File Upload Error";
+          }
+     }
+ }
+ ?>
