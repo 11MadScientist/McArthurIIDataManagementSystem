bc83d32ceb0567bb15b08312c90fd61b18e9a753
febieclinton <72293441+febieclinton@users.noreply.github.com>
2020-11-24 16:45:34 +0800

    added notification button with real-time functionalities

--- includes/topbar.php
+++ includes/topbar.php
@@ -1,4 +1,10 @@
-
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> -->
+<script src="js/scripts.js"></script>
 <nav style="background-color:#03C04A;" id = "topbar" class="sb-topnav navbar navbar-expand navbar-dark background">
     <a class="navbar-brand" href="dashboard.php">MDDMS</a>
     <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
@@ -11,7 +17,73 @@
             </div>
         </div>
     </form>
-    <!-- Navbar-->
+    <!-- NOTIFICATION HEADER -->
+    <ul class="navbar-nav mr-auto" >
+          <li class="nav-item dropdown" >
+            <a class="nav-link" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Notifications 
+                    <?php
+                    // COUNT ALL UNREAD NOTIFICATIONS
+                    $objNotif = new Notifications();
+                    $id = $_SESSION['user_id'];
+                    $query = "SELECT * from `notifications` where `status` = 'unread' and `user_id` = $id order by `date` DESC";
+                    if(count($objNotif->fetchAll($query))>0){
+                    ?>
+                    <span class="badge badge-light"><?php echo count($objNotif->fetchAll($query)); ?></span>
+                    <?php
+                    }
+                        ?>
+            </a>
+
+            <!-- DROPDOWN LIST -->
+            <div class="dropdown-menu" aria-labelledby="dropdown01" style="overflow-x: auto; height:400px"  id="notifList">
+                <?php
+                $objNotif = new Notifications();
+                $id = $_SESSION['user_id'];
+                $query = "SELECT * from `notifications` where `user_id` = $id order by `date` DESC";
+                 if(count($objNotif->fetchAll($query))>0){
+                     foreach($objNotif->fetchAll($query) as $i){
+                         ?>
+              <a style ="
+                         <?php
+                            if($i['status']=='unread'){
+                                echo "font-weight:bold;";
+                            }
+                         ?>
+                         " class="dropdown-item" onclick="updateNotif()"
+                         <?php
+                            echo "id=".$i['id'];
+                            if($i['type']=='announcement')
+                                echo "\nhref=viewAnn.php?id=".$i['type_id'];
+                            else if($i['type']=='event')
+                                echo "\nhref=viewEvent.php?id=".$i['type_id'];
+                            else if($i['type']=='report')
+                                echo "\nhref=reports.php?id=".$i['type_id'];
+                         ?>
+                >
+                    <small><i><?php echo date('F j, Y, g:i a',strtotime($i['date'])) ?></i></small><br/>
+                    <?php 
+                    
+                    if($i['type']=='announcement')
+                        echo "New Announcement";
+                    else if($i['type']=='event')
+                        echo "New Event";
+                    else if($i['type']=='report')
+                        echo "New Report";
+                    ?>
+                </a>
+                <div class="dropdown-divider"></div>
+                <?php
+                    }
+                 }
+                 else
+                 {
+                     echo "No Notifications";
+                 }
+                ?>
+            </div>
+          </li>
+    </ul>
+
     <ul class="navbar-nav ml-auto ml-md-0">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
@@ -24,3 +96,14 @@
         </li>
     </ul>
 </nav>
+
+<script>
+    $(document).ready(function(){
+        $('a.dropdown-item').on('click',function(){
+            var id = this.id;
+            $.post('updateNotif.php' ,{
+                newID: id
+            })
+        });
+    });
+</script>
